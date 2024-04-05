609a1a21f582840b62ab003c3d99855a1da16040
juanda1612 <juan.perez77@educa.madrid.org>
2024-04-05 20:48:58 +0200

    assdgdxastdd

--- TerceraEvaluacion/src/main/java/ejercicios/ejerciciosLamda/ejercicio2/Main.java
+++ TerceraEvaluacion/src/main/java/ejercicios/ejerciciosLamda/ejercicio2/Main.java
@@ -2,9 +2,14 @@ package ejercicios.ejerciciosLamda.ejercicio2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
+    static Scanner teclado = new Scanner(System.in);
     public static void main(String[] args) {
+
+
         Paciente paciente1 = new Paciente("Felipe R", 54,new String[]{"Nauseas", "Mareos"});
         Paciente paciente2 = new Paciente("Irene M", 32,new String[]{"Tos", "Cansancio", "Mareos"});
         Paciente paciente3 = new Paciente("Carmen C", 15,new String[]{"Ansiedad"});
@@ -18,23 +23,99 @@ public class Main {
         pacientes.add(paciente4);
         pacientes.add(paciente5);
 
+        int n;
+        do {
+            System.out.println("Selecciona un metodo de ordenacion");
+            System.out.println("1. Por nombre");
+            System.out.println("2. Por edad");
+            System.out.println("3. Por numero de sintomas");
+            System.out.println("0. Salir");
+            n = teclado.nextInt();
+            switch (n) {
+                case 1:
+                    System.out.println("Ascendente o descendente?");
+                    System.out.println("1. ASC");
+                    System.out.println("2. DES");
+                    System.out.println("0. Salir");
+                    n = teclado.nextInt();
+                    switch (n) {
+                        case 1:
+                            System.out.println("\nORDENAR POR NOMBRE ASC");
+                            pacientes.sort((o1, o2) -> o2.nombre.compareTo(o1.nombre));
+                            for (Paciente elemento : pacientes) {
+                                System.out.println(elemento);
+                            }
+                            break;
+                        case 2:
+                            System.out.println("\nORDENAR POR NOMBRE DES");
+                            pacientes.sort((o1, o2) -> o1.nombre.compareTo(o2.nombre));
+                            for (Paciente elemento : pacientes) {
+                                System.out.println(elemento);
+                            }
+                            break;
+                        case 3:
+                            break;
+                    }
+                case 2:
+                    System.out.println("Ascendente o descendente?");
+                    System.out.println("1. ASC");
+                    System.out.println("2. DES");
+                    System.out.println("0. Salir");
+                    n = teclado.nextInt();
+                    switch (n) {
+                        case 1:
+                            System.out.println("\nORDENAR POR EDAD ASC");
+                            pacientes.sort((o1, o2) -> o1.edad - o2.edad);
+                            break;
+                        case 2:
+                            System.out.println("\nORDENAR POR EDAD DES");
+                            pacientes.sort((o1, o2) -> o2.edad - o1.edad);
+                            break;
+                        case 3:
+                            break;
+                    }
+                case 3:
+                    System.out.println("Ascendente o descendente?");
+                    System.out.println("1. ASC");
+                    System.out.println("2. DES");
+                    System.out.println("0. Salir");
+                    n = teclado.nextInt();
+                    switch (n) {
+                        case 1:
+                            System.out.println("\nORDENAR POR NUMERO DE SINTOMAS ASC");
+                            pacientes.sort((o1, o2) -> o1.getSíntomas().length - o2.getSíntomas().length);
+                            for (Paciente elemento : pacientes) {
+                                System.out.println(elemento);
+                            }
+                            break;
+                        case 2:
+                            System.out.println("\nORDENAR POR NUMERO DE SINTOMAS DES");
+                            pacientes.sort((o1, o2) -> o2.síntomas.length - o1.síntomas.length);
+                            for (Paciente elemento : pacientes) {
+                                System.out.println(elemento);
+                            }
+                            break;
+                        case 3:
+                            break;
+                    }
+                case 4:
+                    break;
+            }
+        }while (n != 0) ;
+        /*
         System.out.println("SIN ORDENAR");
         for (Paciente elemento : pacientes){
             System.out.println(elemento);
         }
 
         System.out.println("\nORDENAR POR NOMBRE ASC");
-        pacientes.sort((o1, o2) ->
-            o1.nombre.compareTo(o2.nombre)
-        );
+        pacientes.sort((o1, o2) -> o2.nombre.compareTo(o1.nombre));
         for (Paciente elemento : pacientes){
             System.out.println(elemento);
         }
 
         System.out.println("\nORDENAR POR NOMBRE DES");
-        pacientes.sort((o1, o2) ->
-                o1.nombre.compareTo(o2.nombre)
-        );
+        pacientes.sort((o1, o2) -> o1.nombre.compareTo(o2.nombre));
         for (Paciente elemento : pacientes){
             System.out.println(elemento);
         }
@@ -82,6 +163,7 @@ public class Main {
             System.out.println(elemento);
         }
 
+
         System.out.println("\nORDENAR POR NUMERO DE SINTOMAS DES");
         pacientes.sort((o1, o2) -> {
             if (o1.síntomas.length < o2.síntomas.length){
@@ -94,6 +176,7 @@ public class Main {
         });
         for (Paciente elemento : pacientes){
             System.out.println(elemento);
-        }
+
+ */
     }
 }
